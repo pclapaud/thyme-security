7edb3130f16081eb796980d9be05cd7a52970b0e
pclapaud <pclapaud@gmail.com>
2019-02-28 15:41:13 +0100

    -Enregistrement dans la session de l'user lors du login -modification de la methode de changement du mdp pour utilisez la session

--- src/main/java/fr/laerce/thymesecurity/configuration/WebSecurityConfig.java
+++ src/main/java/fr/laerce/thymesecurity/configuration/WebSecurityConfig.java
@@ -4,15 +4,22 @@ import fr.laerce.thymesecurity.security.service.JpaUserDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Projet thyme-security
@@ -27,6 +34,8 @@ import org.thymeleaf.spring4.SpringTemplateEngine;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private JpaUserDetailsService jpaUserDetailsService;
     private Logger log = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private HttpSession httpSession;
 
     @Autowired
     public void setUserDetailsService(JpaUserDetailsService jpaUserDetailsService){
@@ -65,6 +74,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("ADMIN","USER")
                 .authorities("WITHDRAW","DEPOSIT","ADMIN");
         auth.userDetailsService(jpaUserDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+    @EventListener
+    public void savesession(InteractiveAuthenticationSuccessEvent event) { // any spring event
+
+        UserDetails test =  (UserDetails)event.getAuthentication().getPrincipal();
+        httpSession.setAttribute("userName",  test.getUsername());
     }
 
 }
--- src/main/java/fr/laerce/thymesecurity/web/UserController.java
+++ src/main/java/fr/laerce/thymesecurity/web/UserController.java
@@ -4,17 +4,21 @@ package fr.laerce.thymesecurity.web;
 import fr.laerce.thymesecurity.security.dao.GroupDao;
 import fr.laerce.thymesecurity.security.domain.User;
 import fr.laerce.thymesecurity.security.service.JpaUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+
 
 @Controller
 @RequestMapping("")
 public class UserController {
-
+    @Autowired
+    private HttpSession httpSession;
 
     GroupDao groupDao;
     JpaUserService jpaUserService;
@@ -39,11 +43,11 @@ public class UserController {
         return "editpassword";
     }
     @GetMapping("/mod")
-    public String modpassword(@RequestParam("id") long id,@RequestParam("password") String password,@RequestParam("password1") String password1,@RequestParam("password2") String password2){
+    public String modpassword(@RequestParam("password") String password,@RequestParam("password1") String password1,@RequestParam("password2") String password2){
         //on check que les 2 nouveau mdp sont les meme
         if (password1.equals(password2)){
 
-        User user = jpaUserService.findByUserId(id);
+        User user = jpaUserService.findByUserName((String)httpSession.getAttribute("userName"));
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     //on check que l'ancien mdp est le meme que celui de la bdd
         if (passwordEncoder.matches(password, user.getPassword())){
@@ -54,7 +58,7 @@ public class UserController {
         }
 
     }
-        return "redirect:/editpassword/"+id;
+        return "redirect:/home";
     }
     @GetMapping("/mod2")
     public String modpasswordAdmin(@RequestParam("id") long id,@RequestParam("password1") String password1,@RequestParam("password2") String password2){
